8be9d44c4d5fb891c53cf39e4c899f882302c9aa
flcerteam <tantn.it@gmail.com>
2018-05-02 23:23:36 +0700

    commit source

--- resources/lang/en/menu.php
+++ resources/lang/en/menu.php
@@ -2,8 +2,8 @@
 
 return [
 
-	'menu'			=> 'Menu hiển thị',
-	'menus'			=> 'Menu hiển thị',
+	'menu'			=> 'Quản lý menu',
+	'menus'			=> 'Quản lý menu',
 	'menu_id'		=> 'Mã menu',
 	'name'			=> 'Tên menu',
 	'hint_category'	=> 'Chọn một hoặc nhiều danh mục để hiển thị tại menu.',
