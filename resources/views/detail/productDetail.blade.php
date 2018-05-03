1d5c4b207c113c23132b584cba5698ba94d755ed
flcerteam <nhue231095@gmail.com>
2018-05-03 20:43:36 +0700

    commit

--- resources/views/detail/productDetail.blade.php
+++ resources/views/detail/productDetail.blade.php
@@ -6,19 +6,18 @@
         <!--Grid row-->
         <div class="row">
             <!--Grid column-->
-            <div class="col-md-5 col-lg-offset-1">
-                <a href="javascript:void(0)">
+            <div class="col-md-6 detail-img">
+                <a href="javascript:void(0)" class="img-thumbnail">
                     <img src="img/{{ $item->image }}" alt="" class="img-responsive">
                 </a>
             </div>
             <!--Grid column-->
             <!--Grid column-->
-            <div class="col-md-5">
+            <div class="col-md-6">
                 <!--Content-->
-                <div class="p-4">
-                <p class="lead font-weight-bold"><strong>{{ $item->name }}</strong></p>
+                <h3>{{ $item->name }}</h3>
                 <p class="lead">
-                    <span>{{ number_format($item->price) }} VND</span>
+                    <span>Giá: {{ number_format($item->price) }} VND</span>
                     <span>/@if ($item->number_of_unit != 1)
                         {{ number_format($item->number_of_unit,0) }}
                     @endif {{ $item->unit_type }}</span>
@@ -30,7 +29,6 @@
                         <input type="number" name="sl" id="sl" value="1" aria-label="Search" class="form-control" style="width: 80px">
                     </div>
                     @include('page.cart')
-                </div>
                 </div>
                 <!--Content-->
             </div>
