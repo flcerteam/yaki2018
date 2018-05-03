9ffd12e4f80c4b8e39da46b250411e44360009fe
flcerteam <nhue231095@gmail.com>
2018-05-03 19:49:33 +0700

    cm

--- routes/web.php
+++ routes/web.php
@@ -53,7 +53,7 @@ Route::get('/search','SearchController@getSearchResult')->name('search');
 /** auto search key word */
 Route::get('/find', 'SearchController@getSearchHint');
 
-Route::get('product-detail/{id}',[
+Route::get('product-detai-{id}.html',[
     'as'=>'detail',
     'uses'=>'DetailController@getDetail'
   ]);
