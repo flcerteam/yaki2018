6fc741e3777fd36b440eefc71e5c465c337bdb70
flcerteam <tantn.it@gmail.com>
2018-05-12 23:22:51 +0700

    commit source

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -8,9 +8,7 @@
         <div class="row">
             <!-- copyright -->
             <div class="col-md-6">
-                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                <span class="copyright">Copyright @2018 All rights reserved | This web is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">FlcerTeam2.3</a></span>
-                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                <span class="copyright">Copyright © 2018 Yaki | This web is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by FT2.3</span>
             </div>
             <!-- /copyright -->
         </div>
@@ -21,4 +19,3 @@
 
 </footer>
 <!-- /Footer -->
-
