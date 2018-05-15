54b5fccca40fb57983edc8b6d9d09b0a16032bd2
flcerteam <nhue231095@gmail.com>
2018-05-16 00:24:40 +0700

    cm

--- resources/views/nav/subNav.blade.php
+++ resources/views/nav/subNav.blade.php
@@ -1,13 +1,15 @@
+@if (0 < count($menus))
 <nav class="navbar navbar-default">
-    <ul class="main-nav nav navbar-nav sub-nav-yaki">
-      @foreach ($menus as $menu)
-        <li><a href="{{ route('type',[str_slug($menu->name,'-'),$menu->menuId,$menu->categoryId]) }}">{{ $menu->name }}</a></li> 
-      @endforeach
-    </ul>
-    <form  class="form-inline" role="search" method="get" id="searchform" action="search" style="margin-top: 0.5em !important">
-      <div class="beta-comp col-lg-1 col-lg-offset-0 col-xs-10 col-xs-offset-1">
-        <input class="form-control" type="text" name="s" id="s" placeholder="Nhập từ khóa" aria-label="Search">
-        <input type="submit" style="position: absolute; left: -9999px"/>
-      </div>
-    </form>
+  <ul class="main-nav nav navbar-nav sub-nav-yaki">
+    @foreach ($menus as $menu)
+      <li><a href="{{ route('type',[str_slug($menu->name,'-'),$menu->menuId,$menu->categoryId]) }}">{{ $menu->name }}</a></li> 
+    @endforeach
+  </ul>
+  <form  class="form-inline" role="search" method="get" id="searchform" action="search" style="margin-top: 0.5em !important">
+    <div class="beta-comp col-lg-1 col-lg-offset-0 col-xs-10 col-xs-offset-1">
+      <input class="form-control" type="text" name="s" id="s" placeholder="Nhập từ khóa" aria-label="Search">
+      <input type="submit" style="position: absolute; left: -9999px"/>
+    </div>
+  </form>
 </nav>
+@endif
